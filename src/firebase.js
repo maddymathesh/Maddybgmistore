a6e21f153337fbf350044f0d0b852ec831c8f70e
maddymathesh <maddybgmistoreog@gmail.com>
2026-05-11 23:31:42 +0530

    feat: enable browser local persistence for Firebase authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
@@ -14,6 +14,14 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
+setPersistence(auth, browserLocalPersistence)
+  .then(() => {
+    console.log("Persistence enabled");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export default app;
